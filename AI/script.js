93120ee4f98442b2a8eaa66f1363edd3b9e723fa
Shaneyboy18 <sh.vogels@sintlucasedu.nl>
2024-11-12 16:24:51 +0100

    update
    editted the template some more, added project pictures.
    
    TODO:
    Make separate pages for each project.
    check the website for errors and such

--- AI/script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('header nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      target.scrollIntoView({ behavior: 'smooth' });
-    });
-  });
-  
